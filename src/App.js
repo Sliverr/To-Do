6cef4425c53558efebebf4274310e25c6fffcbfc
Dakota <the_radical_rad@hotmail.com>
2019-01-28 14:21:50 -0600

    Added event handler to button

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
+  constructor(props){
+
+    super(props);
+    this.state = {isBtnPressed: false};
+
+
+    this.handleClick = this.handleClick.bind(this);
+
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      isBtnPressed: !state.isBtnPressed
+    }));
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Your Daily Planner</h1>
+        <Boxes />
       </div>
     );
   }
 }
+  
+  class Boxes extends Component{
+
+    render(){
+      return (
+        <div>
+       <input type="text" />
+       <button onClick={this.handleClick}>Submit</button>
+       </div>
+      );
+    }
+  }
 
 export default App;
